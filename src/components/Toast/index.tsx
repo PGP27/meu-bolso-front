aa2205cd9f97474dcd1ee10e68a80ee3ac8adb17
Pedro <peg.gep27@gmail.com>
2022-03-25 15:01:27 -0300

    Latest update

---
+++ src/components/Toast/index.tsx
@@ -0,0 +1,28 @@
+import { MdCheckCircle } from "react-icons/md";
+import { MdError } from "react-icons/md";
+import { CgClose } from "react-icons/cg";
+
+interface ToastProps {
+  type: "success" | "error",
+  message: string,
+  close: any,
+};
+
+const Toast = ({ type, message, close }: ToastProps) => (
+  <div className={`w-80 absolute top-10 right-10 p-1 flex items-center justify-between shadow-lg rounded-lg ${type === "success" ? "bg-green-500" : "bg-red-500"}`}>
+    <div className="flex items-center">
+      {type === "success" && <MdCheckCircle color="white" fontSize={24} className="m-2" />}
+      {type === "error" && <MdError color="white" fontSize={24} className="m-2" />}
+      <p className="text-white text-sm">{message}</p>
+    </div>
+    <button
+      type="button"
+      className={`self-start p-1 rounded-full transition ${type === "success" ? "hover:bg-green-400" : "hover:bg-red-400"}`}
+      onClick={() => close(false)}
+    >
+      <CgClose color="white" fontSize={20} />
+    </button>
+  </div>
+);
+
+export default Toast;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,29 +1,39 @@
 import { GiReceiveMoney } from 'react-icons/gi';
 import { AiOutlineUser } from 'react-icons/ai';
 import { AiOutlineLock } from 'react-icons/ai';
+import Toast from '../components/Toast';
 
 const Login = () => (
-  <div className="h-full w-full flex flex-col items-center justify-center bg-gradient-to-br from-green-600 to-green-900">
+  <div className="h-full w-full flex flex-col items-center justify-center bg-gradient-to-br from-green-600 to-green-900 text-white">
     <div className="flex flex-col items-center justify-center">
-      <GiReceiveMoney className="mb-16" color="white" fontSize={100} />
+      <div className="mb-12 flex flex-col items-center">
+        <h1 className="text-4xl mb-12">Meu bolso</h1>
+        <GiReceiveMoney color="white" fontSize={100} />
+      </div>
       <form className="flex flex-col items-center justify-center">
         <div
           className="w-80 py-1 px-4 my-2 flex items-center border rounded-md"
           onClick={() => document.getElementById('input-text')?.focus()}
         >
           <AiOutlineUser color="white" fontSize={22} />
-          <input id="input-text" type="text" autoComplete="off" placeholder="USUÁRIO" className="flex-1 p-2 rounded-md outline-none bg-transparent text-white placeholder-white" />
+          <input
+            id="input-text"
+            type="text"
+            autoComplete="off"
+            placeholder="USUÁRIO"
+            className="flex-1 p-2 rounded-md outline-none bg-transparent placeholder-white"
+          />
         </div>
         <div
           className="w-80 py-1 px-4 my-2 flex items-center border rounded-md"
           onClick={() => document.getElementById('input-password')?.focus()}
         >
           <AiOutlineLock color="white" fontSize={22} />
-          <input id="input-password" type="password" placeholder="SENHA" className="flex-1 p-2 rounded-md outline-none bg-transparent text-white placeholder-white" />
+          <input id="input-password" type="password" placeholder="SENHA" className="flex-1 p-2 rounded-md outline-none bg-transparent placeholder-white" />
         </div>
         <button type="button" className="w-full shadow-lg py-3 mt-10 mb-4 rounded-md bg-white text-green-900 transition hover:opacity-75">LOGIN</button>
       </form>
-      <button className="self-end text-white hover:underline underline-offset-2">Esqueceu sua senha?</button>
+      <button className="self-end hover:underline underline-offset-2">Esqueceu sua senha?</button>
     </div>
   </div>
 );
